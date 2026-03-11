24772953ac3b069c84b91b9eb9a05a2a14f74fa8
Shah <smssyedmohammedshah@gmail.com>
2026-03-11 02:05:30 +0100

    51% done

--- Labs03/project06/simple_mc4.cpp
+++ Labs03/project06/simple_mc4.cpp
@@ -3,10 +3,6 @@
 #include "vanilla2.h"
 #include "random1.h"
 using namespace std;
-// The only purpose of this function is to estimate the future price in present value terms. 
-// We are basically in a room where there is one spot where the one red marble will land in the final chapter.
-// We are trying to peredict where the marble will end up to price the contract we have .
-//  Lets see how we solve the problem.
 double simple_monte_carlo4(const VanillaOption &the_option,
     double spot,
     double vol,
@@ -21,8 +17,6 @@ double simple_monte_carlo4(const VanillaOption &the_option,
         double running_sum = 0.0;
 
         for (unsigned int i = 0; i < number_of_paths; ++i){ 
-            // We throw the marbles a million times . 
-            // And average our wins
             double this_gaussian = get_one_gaussian_by_box_muller();
             this_spot = moved_spot * exp(root_variance * this_gaussian);
             double this_payoff = the_option.get_option_payoff(this_spot);
